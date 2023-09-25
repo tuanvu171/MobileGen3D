03ec0c12405bbb917cafb394b2a454afd60642ce
tuanvu171 <65889368+tuanvu171@users.noreply.github.com>
2023-09-24 23:54:01 -0400

    Update export-onnx.sh

--- script/export-onnx.sh
+++ script/export-onnx.sh
@@ -5,12 +5,12 @@ python3 -m torch.distributed.launch --nproc_per_node=1 --use_env  main.py \
     --project_name $scene \
     --dataset_type Colmap \
     --pseudo_dir model/teacher/ngp_pl/Pseudo/$scene \
-    --root_dir dataset \
+    --root_dir dataset/nerf_data \
     --export_onnx \
-    --input_height 100 \
-    --input_width 100 \
-    --output_height 800 \
-    --output_width 800 \
+    --input_height 64 \
+    --input_width 64 \
+    --output_height 512 \
+    --output_width 512 \
     --scene $scene \
     --ff \
     --ndc \
