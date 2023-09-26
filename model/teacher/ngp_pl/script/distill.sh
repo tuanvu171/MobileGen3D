04c4b1afb4641c402699a7e79ab404749e4954df
tuanvu171 <65889368+tuanvu171@users.noreply.github.com>
2023-09-25 20:10:48 -0400

    Update distill.sh

--- model/teacher/ngp_pl/script/distill.sh
+++ model/teacher/ngp_pl/script/distill.sh
@@ -6,7 +6,7 @@ scene=$1
 python3 train.py \
     --root_dir $ROOT_DIR/$scene \
     --exp_name Pseudo_$scene --dataset_name colmap\
-    --scale 1 --downsample 1 --ff \
+    --scale 1 --downsample 1\
     --save_pseudo_data \
     --n_pseudo_data 8000 --weight_path ckpts/colmap/$scene/epoch=24_slim.ckpt \
     --save_pseudo_path Pseudo/$scene --num_gpu 1 --sr_downscale 8
